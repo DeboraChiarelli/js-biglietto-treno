69510b63837c36e2b5aab7f365ce48badf98cb1c
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-04 18:36:59 +0200

    Add - calcolo sconti & cifra con due decimali

--- app.js
+++ app.js
@@ -26,8 +26,25 @@ console.log(saleForUnder18);
 //- calcolare lo sconto del 40% per gli over 65:
 let saleForOver65 = (fullPrice * percForOver65) / 100;
 console.log(saleForOver65);
-
+//- Calcolare il prezzo finale del viaggio:
+let finalPrice
+//  - Applicare sconto del 20% per gli under 18:
+//      - SE il passeggero sarà minorenne
+        if (age < 18) {
+//      applicherò lo sconto per gli Under18
+        finalPrice = fullPrice - saleForUnder18
+        } else if (age > 65) { //- SE il passeggero sarà over 65
+        //- applicherò lo socnto per gli Over65
+        finalPrice = fullPrice - saleForOver65
+        } else { //- SE il passeggero è tra i 18 e i 65 anni
+        finalPrice = fullPrice
+        }
+        console.log(finalPrice);
+//- Aggiungere cifra con massimo due decimali
+finalPrice = Number(finalPrice.toFixed(2));
 //- Stampare un messaggio dentro h1:
 //    - inserire nell'html un tag h1 con l'id scelto
 //    - recuperare con js l'elemento del dom con l'id scelto
+        const ticketPriceDomElement = document.getElementById('ticketprice'); 
 //    - modificare l'innerHTML dell'h1 con il valore della variabile dell'id scelto
+        ticketPriceDomElement.innerHTML = ticketprice;
