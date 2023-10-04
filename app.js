7194f8b7d99752b25b621fbbfc8406ea06edea88
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-04 21:55:04 +0200

    Fix - interpunzione, toFixed e innerHTML

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //# Calcolo del prezzo del biglietto del treno
 //- Chiedere all'utente il numero dei chilometri che vuole percorrere:
 //    - dichiarare una variabile
-let distanceKm
+let distanceKm;
 //    - assegnare alla variabile il valore costituito da un prompt
 distanceKm = prompt('Inserisci il numero di chilometri che vuoi percorrere');
 console.log(distanceKm);
 //- Chiedere l'età del passeggero:
 //    - dichiarare una variabile
-let age
+let age;
 //    - assegnare alla variabile il valore costituito da un prompt
 age = prompt('Inserisci la tua età');
 console.log(age);
@@ -27,24 +27,24 @@ console.log(saleForUnder18);
 let saleForOver65 = (fullPrice * percForOver65) / 100;
 console.log(saleForOver65);
 //- Calcolare il prezzo finale del viaggio:
-let finalPrice
+let finalPrice;
 //  - Applicare sconto del 20% per gli under 18:
 //      - SE il passeggero sarà minorenne
         if (age < 18) {
-//      applicherò lo sconto per gli Under18
-        finalPrice = fullPrice - saleForUnder18
+//      - applicherò lo sconto per gli Under18
+        finalPrice = fullPrice - saleForUnder18;
         } else if (age > 65) { //- SE il passeggero sarà over 65
-        //- applicherò lo socnto per gli Over65
-        finalPrice = fullPrice - saleForOver65
+//      - applicherò lo sconto per gli Over65
+        finalPrice = fullPrice - saleForOver65;
         } else { //- SE il passeggero è tra i 18 e i 65 anni
-        finalPrice = fullPrice
+        finalPrice = fullPrice;
         }
         console.log(finalPrice);
 //- Aggiungere cifra con massimo due decimali
-finalPrice = Number(finalPrice.toFixed(2));
+finalPrice = (finalPrice.toFixed(2));
 //- Stampare un messaggio dentro h1:
 //    - inserire nell'html un tag h1 con l'id scelto
 //    - recuperare con js l'elemento del dom con l'id scelto
         const ticketPriceDomElement = document.getElementById('ticketprice'); 
 //    - modificare l'innerHTML dell'h1 con il valore della variabile dell'id scelto
-        ticketPriceDomElement.innerHTML = ticketprice;
+        ticketPriceDomElement.innerHTML = finalPrice;s
